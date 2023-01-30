33bf4b9b1048577f1427c6920aa911540d5b9fdc
Fabio Meneghetti <meneghetti.fabio@gmail.com>
2023-01-30 17:21:27 +0100

    event reference task

--- indico_jpsp_ab/client/app/jpsp-ng.app.js
+++ indico_jpsp_ab/client/app/jpsp-ng.app.js
@@ -1,59 +1,18 @@
-// import createComponent from './create-component'
-//
-// import * as AbstractBooklet from './components/abstract-booklet'
-// import * as CheckButton from './components/check-button'
-// import * as DownloadButton from './components/download-button'
-// import * as DownloadProgress from './components/download-progress'
-//
-// createComponent(AbstractBooklet)
-// createComponent(CheckButton)
-// createComponent(DownloadButton)
-// createComponent(DownloadProgress)
-//
-// document.addEventListener('DOMContentLoaded', () => {
-//
-//     document.querySelector('#jpsp-app').innerHTML =
-//         `<jpsp-abstract-booklet></jpsp-abstract-booklet>`;
-//
-// });
 
 
 import { of, Subject } from 'rxjs';
 import { concatMap } from 'rxjs/operators';
 
 
+import {
+    get_settings,
+    get_ulid,
+    log_data,
+    run_handler
+} from './jpsp-ng.lib';
 
 
-// import { createApp as c } from 'vue'
-// 
-// import RootComponent from './jpsp-ng.root.js'
-// 
-// import SettingsBox from './components/jpsp-ng.settings.js'
-// import DownloadBox from './components/jpsp-ng.download.js'
-// import ReportBox from './components/jpsp-ng.report.js'
 
-
-// document.addEventListener("DOMContentLoaded", () => {
-// 
-//     // c(RootComponent)
-//     // 
-//     //     .component('SettingsBox', SettingsBox)
-//     //     .component('DownloadBox', DownloadBox)
-//     //     .component('ReportBox', ReportBox)
-//     // 
-//     //     .mount('#app')
-// 
-// })
-
-function get_settings() {
-    try {
-        return JSON.parse(
-            document.querySelector('#jpsp-ng-settings').textContent
-        );
-    } catch (e) {
-        return undefined;
-    }
-}
 
 
 
@@ -73,12 +32,8 @@ document.addEventListener("DOMContentLoaded", () => {
 
     if (!jpsp_ng_element) { return; }
 
-    document
-        .querySelectorAll(".jpsp")
-        .forEach((e) => (e.style.visibility = "visible"));
 
-
-    const ulid = ULID();
+    const ulid = get_ulid();
 
     const store = {};
 
@@ -110,9 +65,9 @@ document.addEventListener("DOMContentLoaded", () => {
                     ws.addEventListener("message", ({ data }) => {
                         const { head, body } = JSON.parse(data);
 
-                        log_data({ head, body });
+                        log_data({ head, body, store });
 
-                        run_handler({ head, body });
+                        run_handler({ head, body, store });
                     });
 
                     return ok(ws)
@@ -159,69 +114,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
         return { socket, task };
 
-        function run_handler({ head, body, ws }) {
-            if (head.code === "task:progress") {
-                if (head.uuid in store) {
-                    if ("progress" in store[head.uuid]) {
-                        store[head.uuid].progress.call(null, { head, body });
-                    }
-                }
-            }
-            if (head.code === "task:result") {
-                if (head.uuid in store) {
-                    if ("result" in store[head.uuid]) {
-                        store[head.uuid].result.call(null, { head, body });
-                    }
-                }
-            }
-
-            if (head.code === "task:end") {
-                if (head.uuid in store) {
-                    if ("post" in store[head.uuid]) {
-                        store[head.uuid].post.call(null, { head, body });
-                    }
-                    if ("ws" in store[head.uuid]) {
-                        store[head.uuid].ws.close();
-                    }
-                    delete store[head.uuid];
-                }
-            }
-        }
-
-        function log_data({ head, body }) {
-            if (head) store[head.uuid] = store[head.uuid] || {};
-
-            if (["task:queued"].includes(head.code)) {
-                store[head.uuid].queued_time = head.time;
-            } else if (["task:begin"].includes(head.code)) {
-                store[head.uuid].begin_time = head.time;
-            } else if (["task:result"].includes(head.code)) {
-                store[head.uuid].result_time = head.time;
-            } else if (["task:end", "task:error"].includes(head.code)) {
-                store[head.uuid].end_time = head.time;
-
-                console.log(
-                    `[${head.uuid}]`,
-                    "queued -> end",
-                    store[head.uuid].end_time - store[head.uuid].queued_time,
-                    "seconds"
-                );
-
-                console.log(
-                    `[${head.uuid}]`,
-                    "begin -> end",
-                    store[head.uuid].end_time - store[head.uuid].begin_time,
-                    "seconds"
-                );
-
-                console.log(
-                    `[${head.uuid}]`,
-                    "queued -> begin",
-                    store[head.uuid].begin_time - store[head.uuid].queued_time,
-                    "seconds"
-                );
-            }
-        }
+        
     })(jpsp_ng_settings);
 
 
@@ -296,6 +189,41 @@ document.addEventListener("DOMContentLoaded", () => {
             err: (e) => console.error(e),
             params: () => fetchEventJson(),
             method: "event_ab",
+        });
+    });
+
+
+
+
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    /////////////// ABSTRACT BOOKLET //////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////////////////////
+
+
+
+
+    document.querySelector(".reference").addEventListener("click", (ev) => {
+        const loader = createLoader("Downloading...");
+
+        ws.task({
+            pre: () => {
+                ev.target.classList.add("disabled");
+                ev.target.appendChild(loader);
+            },
+            progress: ({ head, body }) => {
+                console.log(body?.params)
+            },
+            result: ({ head, body }) => {
+                ev.target.classList.remove("disabled");
+                loader.remove();
+            },
+            err: (e) => console.error(e),
+            params: () => fetchEventJson(),
+            method: "event_ref",
         });
     });
 
@@ -674,84 +602,5 @@ function download(body) {
         a.remove();
         window.URL.revokeObjectURL(url);
     });
-}
-
-function ULID() {
-    const BASE32 = [
-        "0",
-        "1",
-        "2",
-        "3",
-        "4",
-        "5",
-        "6",
-        "7",
-        "8",
-        "9",
-        "A",
-        "B",
-        "C",
-        "D",
-        "E",
-        "F",
-        "G",
-        "H",
-        "J",
-        "K",
-        "M",
-        "N",
-        "P",
-        "Q",
-        "R",
-        "S",
-        "T",
-        "V",
-        "W",
-        "X",
-        "Y",
-        "Z",
-    ];
-    let last = -1;
-    /* Pre-allocate work buffers / views */
-    let ulid = new Uint8Array(16);
-    let time = new DataView(ulid.buffer, 0, 6);
-    let rand = new Uint8Array(ulid.buffer, 6, 10);
-    let dest = new Array(26);
-
-    function encode(ulid) {
-        dest[0] = BASE32[ulid[0] >> 5];
-        dest[1] = BASE32[(ulid[0] >> 0) & 0x1f];
-        for (let i = 0; i < 3; i++) {
-            dest[i * 8 + 2] = BASE32[ulid[i * 5 + 1] >> 3];
-            dest[i * 8 + 3] =
-                BASE32[((ulid[i * 5 + 1] << 2) | (ulid[i * 5 + 2] >> 6)) & 0x1f];
-            dest[i * 8 + 4] = BASE32[(ulid[i * 5 + 2] >> 1) & 0x1f];
-            dest[i * 8 + 5] =
-                BASE32[((ulid[i * 5 + 2] << 4) | (ulid[i * 5 + 3] >> 4)) & 0x1f];
-            dest[i * 8 + 6] =
-                BASE32[((ulid[i * 5 + 3] << 1) | (ulid[i * 5 + 4] >> 7)) & 0x1f];
-            dest[i * 8 + 7] = BASE32[(ulid[i * 5 + 4] >> 2) & 0x1f];
-            dest[i * 8 + 8] =
-                BASE32[((ulid[i * 5 + 4] << 3) | (ulid[i * 5 + 5] >> 5)) & 0x1f];
-            dest[i * 8 + 9] = BASE32[(ulid[i * 5 + 5] >> 0) & 0x1f];
-        }
-        return dest.join("");
-    }
-
-    return function () {
-        let now = Date.now();
-        if (now === last) {
-            /* 80-bit overflow is so incredibly unlikely that it's not
-             * considered as a possiblity here.
-             */
-            for (let i = 9; i >= 0; i--) if (rand[i]++ < 255) break;
-        } else {
-            last = now;
-            time.setUint16(0, (now / 4294967296.0) | 0);
-            time.setUint32(2, now | 0);
-            window.crypto.getRandomValues(rand);
-        }
-        return encode(ulid);
-    };
 }
 
---
+++ indico_jpsp_ab/client/app/jpsp-ng.old.js
@@ -0,0 +1,39 @@
+// import createComponent from './create-component'
+//
+// import * as AbstractBooklet from './components/abstract-booklet'
+// import * as CheckButton from './components/check-button'
+// import * as DownloadButton from './components/download-button'
+// import * as DownloadProgress from './components/download-progress'
+//
+// createComponent(AbstractBooklet)
+// createComponent(CheckButton)
+// createComponent(DownloadButton)
+// createComponent(DownloadProgress)
+//
+// document.addEventListener('DOMContentLoaded', () => {
+//
+//     document.querySelector('#jpsp-app').innerHTML =
+//         `<jpsp-abstract-booklet></jpsp-abstract-booklet>`;
+//
+// });
+
+// import { createApp as c } from 'vue'
+// 
+// import RootComponent from './jpsp-ng.root.js'
+// 
+// import SettingsBox from './components/jpsp-ng.settings.js'
+// import DownloadBox from './components/jpsp-ng.download.js'
+// import ReportBox from './components/jpsp-ng.report.js'
+
+
+// document.addEventListener("DOMContentLoaded", () => {
+// 
+//     // c(RootComponent)
+//     // 
+//     //     .component('SettingsBox', SettingsBox)
+//     //     .component('DownloadBox', DownloadBox)
+//     //     .component('ReportBox', ReportBox)
+//     // 
+//     //     .mount('#app')
+// 
+// })
