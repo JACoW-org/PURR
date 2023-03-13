a8c31455cd38aa7fa00ff8324c2f3ae998fa83ca
Fabio Meneghetti <meneghetti.fabio@gmail.com>
2023-03-13 15:33:59 +0100

    timezone

--- indico_purr/controllers/utils.py
+++ indico_purr/controllers/utils.py
@@ -1,4 +1,4 @@
-from indico_purr.utils import json_decode
+from indico_purr.utils import DEFAULT_TIMEZONE, json_decode
 from flask_pluginengine import current_plugin
 
 
@@ -35,6 +35,7 @@ def get_settings_util(s) -> dict | None:
     return dict(
         api_key=s.api_key,
         api_url=s.api_url,
+        timezone=str(DEFAULT_TIMEZONE),
         pdf_page_width=s.pdf_page_width,
         pdf_page_height=s.pdf_page_height,
         custom_fields=get_contribution_fields(s),
