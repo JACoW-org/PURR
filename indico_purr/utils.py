9d91c2872eca83e8b81e6e06b2ff4e9b915d6dd6
Adrian Moennich <adrian.moennich@cern.ch>
2023-03-23 16:59:23 +0100

    Remove unnecessary dependency

--- indico_purr/utils.py
+++ indico_purr/utils.py
@@ -1,11 +1,10 @@
+import json
 from typing import Any
-
-import orjson
 
 
 def json_decode(data: bytes) -> Any:
-    return orjson.loads(data)
+    return json.loads(data)
 
 
 def json_encode(data: Any) -> bytes:
-    return orjson.dumps(data)
+    return json.dumps(data)
