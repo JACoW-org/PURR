8f2109fb2313fa416a122be1d86b89d41a8750d8
Adrian Moennich <adrian.moennich@cern.ch>
2023-03-23 16:57:24 +0100

    Remove unused utils
    They were also broken (w/ exceptions being silenced) because the config
    cannot be accessed at import time.

--- indico_purr/utils.py
+++ indico_purr/utils.py
@@ -1,19 +1,6 @@
 from typing import Any
 
 import orjson
-from flask_pluginengine import current_plugin
-from pytz import timezone
-
-from indico.core.config import config
-
-
-def default_timezone():
-    try:
-        if not config is None and not config.DEFAULT_TIMEZONE is None:
-            return timezone(config.DEFAULT_TIMEZONE)
-    except BaseException as e:
-        current_plugin.logger.error(e)
-    return timezone('UTC')
 
 
 def json_decode(data: bytes) -> Any:
@@ -22,6 +9,3 @@ def json_decode(data: bytes) -> Any:
 
 def json_encode(data: Any) -> bytes:
     return orjson.dumps(data)
-
-
-DEFAULT_TIMEZONE = default_timezone()
