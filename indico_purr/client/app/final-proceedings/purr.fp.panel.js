a2264ea34945ad864e50d6c760842a3106d3a8fd
Christian <christian.obetti@akera.it>
2023-08-08 15:14:25 +0200

    Handled tasks lists

--- indico_purr/client/app/final-proceedings/purr.fp.panel.js
+++ indico_purr/client/app/final-proceedings/purr.fp.panel.js
@@ -7,8 +7,9 @@ import {PurrErrorAlert} from '../purr.error.alert';
 
 const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setFPInfo}) => {
   const [processing, setProcessing] = useState(() => false);
-  const [prePressProcessing, setPrePressProcessing] = useState(false);
-  const [finalProcProcessing, setFinalProcProcessing] = useState(false);
+  const [prePressProcessing, setPrePressProcessing] = useState(() => false);
+  const [finalProcProcessing, setFinalProcProcessing] = useState(() => false);
+  const [currentTaskIndex, setCurrentTaskIndex] = useState(() => 0);
   const [taskCount, setTaskCount] = useState(() => 0);
 
   const [ops, setOps] = useState(() => []);
@@ -17,7 +18,7 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
   const [downloadProceedings, setDownloadProceedings] = useState(() => false);
   const [showError, setShowError] = useState(() => false);
   const [errorMessage, setErrorMessage] = useState(() => null);
-  const [progressStatus, setProgressStatus] = useState(() => null);   // 'success' || 'error' || 'active' || 'aborted'
+  const [progressStatus, setProgressStatus] = useState(() => null); // 'success' || 'error' || 'active' || 'aborted'
 
   const onClose = useCallback(() => {
     setOpen(false);
@@ -26,17 +27,16 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
   }, []);
 
   const onAbort = useCallback(() => {
-    // console.log('onAbort', prePressProcessing, finalProcProcessing)
-
     setProgressStatus('aborted');
+    setOps(tasks =>
+      tasks.map(task => (task.running ? {...task, running: false, icon: 'close'} : task))
+    );
 
     if (prePressProcessing) {
-      // console.log('onAbort - prePressProcessing')
       setPrePressProcessing(false);
     }
 
     if (finalProcProcessing) {
-      // console.log('onAbort - finalProcProcessing')
       setFinalProcProcessing(false);
     }
   }, [prePressProcessing, finalProcProcessing]);
@@ -48,23 +48,6 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
     const url = new URL(`${info?.event_id}`, `${settings?.api_url}`);
     window.open(url, '_blank');
   }, [info, settings]);
-
-  // set task count based on the selected event
-  useEffect(() => {
-    if (compressProceedings) {
-      setTaskCount(2);
-    }
-
-    if (prePressProcessing) {
-      setTaskCount(17);
-    }
-
-    if (finalProcProcessing) {
-      setTaskCount(19);
-    }
-
-    return () => {};
-  }, [compressProceedings, prePressProcessing, finalProcProcessing]);
 
   useEffect(() => {
     setProcessing(
@@ -90,7 +73,6 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
     let [task_id, socket] = [];
 
     if (prePressProcessing || finalProcProcessing) {
-
       setProgressStatus('active');
 
       const method = finalProcProcessing ? 'event_final_proceedings' : 'event_pre_press';
@@ -105,11 +87,30 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
       // actions
       const actions = {
         'task:progress': (head, body) => {
-          if (body?.params?.text) {
-            setOps(prevOps => [
-              ...prevOps.map(o => ({icon: 'check', text: o.text})),
-              {last: true, icon: 'spinner', text: `${body.params.text}`},
-            ]);
+          if (body?.params?.phase === 'init_tasks_list') {
+            setTaskCount(body?.params?.tasks.length);
+            setOps(
+              body?.params?.tasks.map(task => ({
+                icon: '',
+                text: task.text,
+                code: task.code,
+                running: false,
+              }))
+            );
+          } else if (body?.params?.text) {
+            setOps(ops => {
+              const newTaskIndex = ops.findIndex(task => task.code === body.params.phase);
+              if (newTaskIndex > -1) {
+                ops[newTaskIndex].icon = 'spinner';
+                ops[newTaskIndex].running = true;
+                setCurrentTaskIndex(newTaskIndex);
+                if (newTaskIndex > 0) {
+                  ops[newTaskIndex - 1].icon = 'check';
+                  ops[newTaskIndex - 1].running = false;
+                }
+              }
+              return ops;
+            });
           }
         },
         'task:log': (head, body) => {
@@ -118,9 +119,11 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
           }
         },
         'task:result': (head, body) => {
-          console.log(head, body);
 
-          setOps(prevOps => [...prevOps.map(o => ({icon: 'check', text: o.text}))]);
+          setOps(prevOps => {
+            setCurrentTaskIndex(prevOps.length);
+            return [...prevOps.map(o => ({...o, icon: 'check', running: false}))];
+          });
 
           setProgressStatus('success');
 
@@ -155,7 +158,7 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
         complete: () => {
           setPrePressProcessing(false);
           setFinalProcProcessing(false);
-          setProgressStatus(current => current === 'active' ? 'error' : current);
+          setProgressStatus(current => (current === 'active' ? 'error' : current));
         },
         error: err => {
           console.error(err);
@@ -225,7 +228,7 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
       if (socket) {
         socket.complete();
 
-        setOps(prevOps => [...prevOps.map(o => ({icon: 'check', text: o.text})), {text: ''}]);
+        // setOps(prevOps => [...prevOps.map(o => ({...o, icon: 'check', running: false}))]);
       }
     };
   }, [prePressProcessing, finalProcProcessing]);
@@ -233,7 +236,6 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
   // compress proceedings
   useEffect(() => {
     if (compressProceedings) {
-
       setProgressStatus('active');
 
       const method = 'event_compress_proceedings';
@@ -242,11 +244,30 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
 
       const actions = {
         'task:progress': (head, body) => {
-          if (body?.params?.text) {
-            setOps(prevOps => [
-              ...prevOps.map(o => ({icon: 'check', text: o.text})),
-              {last: true, icon: 'spinner', text: `${body.params.text}`},
-            ]);
+          if (body?.params?.phase === 'init_tasks_list') {
+            setTaskCount(body?.params?.tasks.length);
+            setOps(
+              body?.params?.tasks.map(task => ({
+                icon: '',
+                text: task.text,
+                code: task.code,
+                running: false,
+              }))
+            );
+          } else if (body?.params?.text) {
+            setOps(ops => {
+              const newTaskIndex = ops.findIndex(task => task.code === body.params.phase);
+              if (newTaskIndex > -1) {
+                ops[newTaskIndex].icon = 'spinner';
+                ops[newTaskIndex].running = true;
+                setCurrentTaskIndex(newTaskIndex);
+                if (newTaskIndex > 0) {
+                  ops[newTaskIndex - 1].icon = 'check';
+                  ops[newTaskIndex - 1].running = false;
+                }
+              }
+              return ops;
+            });
           }
         },
         'task:log': (head, body) => {
@@ -257,7 +278,10 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
         'task:result': (head, body) => {
           console.log(head, body);
 
-          setOps(prevOps => [...prevOps.map(o => ({icon: 'check', text: o.text}))]);
+          setOps(prevOps => {
+            setCurrentTaskIndex(prevOps.length);
+            return [...prevOps.map(o => ({...o, icon: 'check', running: false}))];
+          });
 
           setProgressStatus('success');
           setFPInfo(prev => ({...prev, proceedings_archive: true}));
@@ -284,8 +308,8 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
       socket.subscribe({
         next: ({head, body}) => runPhase(head, body, actions, socket),
         complete: () => {
-          setCompressProceedings(false);          
-          setProgressStatus(current => current === 'active' ? 'error' : current);
+          setCompressProceedings(false);
+          setProgressStatus(current => (current === 'active' ? 'error' : current));
         },
         error: err => {
           console.error(err);
@@ -340,8 +364,6 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
       return () => {
         if (socket) {
           socket.complete();
-
-          setOps(prevOps => [...prevOps.map(o => ({icon: 'check', text: o.text})), {text: ''}]);
         }
       };
     }
@@ -368,7 +390,7 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
           {ops.length > 0 ? (
             ops.map((op, key) => (
               <div key={key}>
-                <Icon loading={!!op.last} name={op.icon} />
+                <Icon loading={op.running} name={op.icon} />
                 <span>{op.text}</span>
               </div>
             ))
@@ -388,7 +410,7 @@ const FinalProcPanel = ({open, setOpen, info, settings, eventTitle, fpInfo, setF
           <Progress
             color="blue"
             disabled={!ops?.length}
-            value={ops?.length - 1}
+            value={currentTaskIndex}
             total={taskCount}
             progress="ratio"
             active={progressStatus === 'active'}
