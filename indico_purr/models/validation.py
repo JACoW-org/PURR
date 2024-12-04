6b06d8d1b5594182fe006f1b2e19db1088d3d183
Adrian Moennich <adrian.moennich@cern.ch>
2024-12-04 14:44:17 +0100

    Reformat file that had 2-space indentation

--- indico_purr/models/validation.py
+++ indico_purr/models/validation.py
@@ -4,27 +4,27 @@ from typing import Any, Callable, Optional
 
 @dataclass
 class Validation:
-  """ Class to use to validate a DTO"""
+    """Class to use to validate a DTO"""
 
-  errors: dict = field(default_factory=dict)
+    errors: dict = field(default_factory=dict)
 
-  def required(self, field: str, value) -> 'Validation':
-    if isinstance(value, list) and len(value) == 0:
-      self.errors[field] = 'error.required'
-    elif not value:
-      self.errors[field] = 'error:required'
-    return self
-  
-  def not_empty(self, field: str, list) -> 'Validation':
-    if len(list) == 0:
-      self.errors[field] = 'error.not-empty'
-    return self
-  
-  def custom_validator(self, field: str, value, validator: Callable[[str, Any], Optional[str]]) -> 'Validation':
-    error = validator(value)
-    if error:
-      self.errors[field] = error
-    return self
+    def required(self, field: str, value) -> 'Validation':
+        if isinstance(value, list) and len(value) == 0:
+            self.errors[field] = 'error.required'
+        elif not value:
+            self.errors[field] = 'error:required'
+        return self
 
-  def get_errors(self) -> dict:
-    return self.errors
+    def not_empty(self, field: str, list) -> 'Validation':
+        if len(list) == 0:
+            self.errors[field] = 'error.not-empty'
+        return self
+
+    def custom_validator(self, field: str, value, validator: Callable[[str, Any], Optional[str]]) -> 'Validation':
+        error = validator(value)
+        if error:
+            self.errors[field] = error
+        return self
+
+    def get_errors(self) -> dict:
+        return self.errors
